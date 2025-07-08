00d101ef3e8d382c3227d270db09335c66edd5cb
shun298104 <hentaime@example.com>
2025-07-08 21:44:02 +0900

    commit before push gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path' // ← これを追加！
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
